463a9c3ca7136f45aab8cd8792b4c9ce167fa7f6
DrMaemi <leesh1510@ajou.ac.kr>
2021-07-28 16:07:21 +0900

    ReID 결과를 행동 인식 모델과 결합, 시각화 반영 - 불안정

--- demo/demo.py
+++ demo/demo.py
@@ -8,8 +8,10 @@ from time import sleep
 from itertools import count
 from tqdm import tqdm
 
+import numpy as np
 import torch
 from reid import REID
+import operator
 
 from visualizer import AVAVisualizer
 from action_predictor import AVAPredictorWorker
@@ -96,13 +98,13 @@ def main():
     )
     parser.add_argument(
         "--tracker-box-thres",
-        default=0.1,
+        default=0.3,
         help="The box threshold for tracker",
         type=float,
     )
     parser.add_argument(
         "--tracker-nms-thres",
-        default=0.4,
+        default=0.6,
         help="The nms threshold for tracker",
         type=float,
     )
@@ -146,12 +148,17 @@ def main():
 
     ava_predictor_worker = AVAPredictorWorker(args)
     pred_done_flag = False
+
+    track_cnt = dict()
     images_by_id = dict()
+    ids_per_frame = []
+    
     print("Showing tracking progress bar (in fps). Other processes are running in the background.")
     try:
         for i in tqdm(count(), desc="Tracker Progress", unit=" frame"):
             with torch.no_grad():
                 (frame, orig_img, boxes, scores, ids) = ava_predictor_worker.read_track()
+                # print("type(frame) = {}".format(type(frame))) # <class 'numpy.ndarray'>
 
                 if orig_img is None:
                     if not args.realtime:
@@ -165,18 +172,22 @@ def main():
                         break
                 else:
                     # print("demo.py - get images_by_id")
-                    for bbox, id in zip(boxes, map(int, ids)):
-                        if id not in images_by_id:
-                            # images_by_id[id] = [frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]]
-                            images_by_id[id] = [frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]]
-                        else:
-                            # images_by_id[id].append(frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
-                            images_by_id[id].append(frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
-                    # for id in images_by_id.keys():
-                    #     print("id: {0}, type(images_by_id[{0}]): {1}".format(id, type(images_by_id[id])))
-                    #     last = len(images_by_id[id])-1
-                    #     print("len(images_by_id[{}]): {}".format(id, last+1))
-                    #     print("images_by_id[{}][{}].shape = {}".format(id, last, images_by_id[id][last].shape))
+                    try:
+                        ids_per_frame.append(set(map(int, ids)))
+                        for bbox, id in zip(boxes, map(int, ids)):
+                            if id not in images_by_id:
+                                # images_by_id[id] = [frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]]
+                                images_by_id[id] = [frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]]
+                            else:
+                                # images_by_id[id].append(frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
+                                images_by_id[id].append(frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])])
+                        # for id in images_by_id.keys():
+                        #     print("id: {0}, type(images_by_id[{0}]): {1}".format(id, type(images_by_id[id])))
+                        #     last = len(images_by_id[id])-1
+                        #     print("len(images_by_id[{}]): {}".format(id, last+1))
+                        #     print("images_by_id[{}][{}].shape = {}".format(id, last, images_by_id[id][last].shape))
+                    except TypeError:
+                        pass
                     video_writer.send_track((boxes, ids))
                     while not pred_done_flag:
                         result = ava_predictor_worker.read()
@@ -190,14 +201,97 @@ def main():
         print("Keyboard Interrupted")
 
     if not args.realtime:
+        threshold = 320
+        exist_ids = set()
+        final_fuse_id = dict()
+
+        print('Total IDs = ',len(images_by_id))
+        feats = dict()
+        for i in images_by_id:
+            # print('ID number {} -> Number of frames {}'.format(i, len(images_by_id[i])))
+            # print("type(images_by_id[{}] = {}".format(i, type(images_by_id[i])))
+            # print("type(images_by_id[{}][0] = {}".format(i, type(images_by_id[i][0])))
+            # print("type(images_by_id[{}]) = {}".format(i, type(images_by_id[i])))
+            # print("type(images_by_id[{}][0] = {}".format(i, type(images_by_id[i][0])))
+            try:
+                feats[i] = reid._features(images_by_id[i])
+                # print("type(feats[i]) = {}".format(type(feats[i])))
+                # print("feats[i] = {}".format(feats[i]))
+            except ValueError:
+                pass
+
+        for f in ids_per_frame:
+            if f:
+                if len(exist_ids) == 0:
+                    for i in f:
+                        final_fuse_id[i] = [i]
+                    # print("type(exist_ids) = {}".format(type(exist_ids))) # set
+                    # print("exist_ids = {}".format(exist_ids)) # {1, 2, 3, 4}, ...?
+                    # print("type(f) = {}".format(type(f))) # set
+                    # print("f = {}".format(f)) # {1, 2} ...?
+                    exist_ids = exist_ids or f
+                else:
+                    new_ids = f-exist_ids
+                    for nid in new_ids:
+                        dis = []
+                        if len(images_by_id[nid])<10:
+                            exist_ids.add(nid)
+                            continue
+                        unpickable = []
+                        for i in f:
+                            for key,item in final_fuse_id.items():
+                                if i in item:
+                                    unpickable += final_fuse_id[key]
+                        print('exist_ids {} unpickable {}'.format(exist_ids,unpickable))
+                        for oid in (exist_ids-set(unpickable))&set(final_fuse_id.keys()):
+                            try:
+                                tmp = np.mean(reid.compute_distance(feats[nid],feats[oid]))
+                                print('nid {}, oid {}, tmp {}'.format(nid, oid, tmp))
+                                dis.append([oid, tmp])
+                            except KeyError:
+                                pass
+                        exist_ids.add(nid)
+                        print("type(nid) = {}".format(type(nid)))
+                        print("nid = {}".format(nid))
+                        if not dis:
+                            final_fuse_id[nid] = [nid]
+                            continue
+                        dis.sort(key=operator.itemgetter(1))
+                        if dis[0][1] < threshold:
+                            combined_id = dis[0][0]
+                            images_by_id[combined_id] += images_by_id[nid]
+                            final_fuse_id[combined_id].append(nid)
+                        else:
+                            final_fuse_id[nid] = [nid]
+        # print("demo.py - main()")
+        # print("type(final_fuse_id) = {}".format(type(final_fuse_id)))
+        print('Final ids and their sub-ids:', final_fuse_id)
+        final_fuse_id_reverse = dict()
+        for final_id, sub_ids in final_fuse_id.items():
+            for sub_id in sub_ids:
+                final_fuse_id_reverse[sub_id] = final_id
+        # print("final_fuse_id_reverse = {}".format(final_fuse_id_reverse))
+
+        # print('MOT took {} seconds'.format(int(time.time() - t1)))
+        # t2 = time.time()
+
         video_writer.send_track("DONE")
+        print("demo.py - main()")
         while not pred_done_flag:
             result = ava_predictor_worker.read()
+            # print("type(result = ava_predictor_worker.read()) = {}".format(type(result)))
+            # print("ava_predictor_worker.output_queue.get_nowait(): (predictions, center_timestamp, ids): result = {}".format(result))
             if result is None:
                 sleep(0.1)
             elif result == "done":
                 pred_done_flag = True
             else:
+                for idx, id in enumerate(result[2]): # ids
+                    id = int(id)
+                    # print("BEFORE: result[2][{}] = {}".format(idx, result[2][idx]))
+                    if id in final_fuse_id_reverse:
+                        result[2][idx] = final_fuse_id_reverse[id]
+                    # print("AFTER: result[2][{}] = {}".format(idx, result[2][idx]))
                 video_writer.send(result)
 
         video_writer.send("DONE")
